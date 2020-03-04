d847101c7cc0dd82388882daa81c26abb4f0176f
Sean Melton <seanlape@gmail.com>
2020-03-04 18:52:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["wow", "Aard", "Igni", "arrays!"]
+  ["wow", "Aard", "am", "arrays!"]
 end
 
 def add_element_to_end_of_array(array, element)
